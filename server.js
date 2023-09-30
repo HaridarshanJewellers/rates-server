1db238efac1198d5acf57f69e8debd0d3c2bbf2f
HaridarshanJewellers <146411605+HaridarshanJewellers@users.noreply.github.com>
2023-09-30 09:45:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ let app = express();
 app.use(cors({ origin: "*" }));
 
 
-res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
-res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+app.use(cors({ origin: "https://haridarshanjewellers.github.io/rate" }));
 
 
 const axios = function (url) {
