90b5bc37955e724a22266a385dd906f00efbf50d
HaridarshanJewellers <146411605+HaridarshanJewellers@users.noreply.github.com>
2023-10-02 08:48:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,26 +18,22 @@ app.use(async (req, res, next) => {
 
 const axios = function (url) {
   return new Promise((resolve, reject) => {
-    return http.get(
-      "http://bcast.rakshabullion.com:7767/VOTSBroadcastStreaming/Services/xml/GetLiveRateByTemplateID/raksha?_=" +
-        Date.now(),
-      function (response) {
-        if (response.statusCode === 200) {
-          const data = [];
-          response.on("data", (chunk) => {
-            data.push(chunk);
-          });
+    return http.get(url + Date.now(), function (response) {
+      if (response.statusCode === 200) {
+        const data = [];
+        response.on("data", (chunk) => {
+          data.push(chunk);
+        });
 
-          response.on("end", () => {
-            resolve(data.toString());
-          });
+        response.on("end", () => {
+          resolve(data.toString());
+        });
 
-          response.on("error", () => {
-            reject("Request failed!");
-          });
-        }
+        response.on("error", () => {
+          reject("Request failed!");
+        });
       }
-    );
+    });
   });
 };
 
@@ -45,10 +41,11 @@ const _goldDigger = async function () {
   try {
     const date = Date.now();
     const response = await axios(
-      "http://bcast.rakshabullion.com:7767/VOTSBroadcastStreaming/Services/xml/GetLiveRateByTemplateID/raksha?_=" +
+      "http://bcast.kakagold.in:7767/VOTSBroadcastStreaming/Services/xml/GetLiveRateByTemplateID/kaka?_=" +
         date
     );
 
+    console.log(response);
     let gold = 0;
     // For gold
     response.split("\n").map((line) => {
@@ -57,7 +54,7 @@ const _goldDigger = async function () {
         .map((col) => col.trim())
         .filter((col) => !!col);
 
-      if (columns[1] === "GOLD 999 WITH GST") {
+      if (columns[1] === "GOLD 999 IMP WITH GST") {
         gold = parseInt(columns[3]);
       }
     });
